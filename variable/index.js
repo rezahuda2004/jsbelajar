576bded064c4ddf3ab5b9bb985525ccddb6a6321
rezahuda2004 <hudamajor02@gmail.com>
2022-10-24 21:35:26 +0700

    dasar

---
+++ variable/index.js
@@ -0,0 +1,11 @@
+// variable ada 3 di javascript
+// fungsi variable adalah untuk menyimpan data(array, number, integer, boolean, object, string)
+// const -> ketika ga pingin nilai variable berubah maka gunakan ini
+// let -> ketika pingin nilai variable  berubah ubah gunakan ini tidak bisa panggil let lagi
+// var -> ketika mau membuat variable nama yang sama bisa dipanggil var lagi
+
+var jk = "cwo"
+var jk = "cwe"
+console.log(jk)
+
+
